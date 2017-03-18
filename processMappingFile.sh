f8bc1fc3d4401dd4a279e08ee9762e049cd11f10
Nicholas D. Socci <soccin@mskcc.org>
2017-03-18 17:01:18 -0400

    Added main run script

---
+++ processMappingFile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+GENOME=$1
+MAPPING=$2
+
+if [ "$#" -ne 2 ]; then
+    echo "usage processMappingFile.sh GENOME MAPPING_FILE"
+    exit
+fi
+
+if [ -e "$SDIR/genomes/$GENOME" ]; then
+    $GENOMEMOD=$SDIR/genomes/$GENOME
+else
+    $GENOMEMOD=$GENOME
+fi
+
+for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
+    echo $sample;
+    cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
+        | xargs $SDIR/PEMapper/pipe.sh -s $sample $GENOMEMOD
+done
