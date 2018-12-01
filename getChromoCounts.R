1726fa9d77b16ae80713c622581704dfb9be3477
Nicholas D. Socci <soccin@mskcc.org>
2018-12-01 15:57:49 -0500

    Add script to count reads mapping to choromosomes

---
+++ getChromoCounts.R
@@ -0,0 +1,19 @@
+#!/bin/bash
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+MAX_INSERT_SIZE=$1
+BAM=$2
+
+#
+# -f2 Proper Pairing Flag
+# -F2048 !secondary alignments
+
+samtools view -f2 -F2048 -b ${BAM} \
+    | bedtools bamtobed -bedpe -i - \
+    | $SDIR/pebed2fragbed.py $MAX_INSERT_SIZE | bedtools sort -i - \
+    | cut -f1 \
+    | sort -V \
+    | uniq -c \
+    | awk 'BEGIN{print "Contig\tCount"}{print $2"\t"$1}' \
+    > ${BAM/.bam/___Count.txt}
+
