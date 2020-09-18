144d5856e61fad9684c128f0448c15977a151f67
Nicholas D. Socci <soccin@mskcc.org>
2020-09-18 14:43:14 -0400

    Added delivery text

--- deliver.sh
+++ deliver.sh
@@ -2,14 +2,51 @@
 
 ODIR=$1
 
-mkdir $ODIR/alignment
-mkdir $ODIR/coverage
-mkdir $ODIR/metrics
+SPECIESCOUNTS=$(ls | egrep "__SpeciesCounts.xlsx$")
 
-cp *_sample_mapping.txt $ODIR
+mkdir -p $ODIR/alignment
+mkdir -p $ODIR/coverage
+mkdir -p $ODIR/metrics
+
+if [ "$SPECIESCOUNTS" != "" ]; then
+    cp $SPECIESCOUNTS $ODIR/metrics
+fi
+
+MAPPINGFILE=$(find . | egrep "_sample_mapping.txt$")
+if [ "$MAPPINGFILE" != "" ]; then
+    cp *_sample_mapping.txt $ODIR
+fi
 
 rsync -rvP out___/*___MD.ba? $ODIR/alignment
 rsync -rvP out___/*COV.txt $ODIR/coverage
 rsync -rvP out___/*___AS.txt $ODIR/metrics
 rsync -rvP out___/*___MD.txt $ODIR/metrics
 rsync -rvP out___/*___INS.txt $ODIR/metrics
+
+LAB=$(echo $ODIR | cut -d/ -f5)
+INVEST=$(echo $ODIR | cut -d/ -f6)
+PROJNO=$(echo $ODIR | cut -d/ -f7)
+SAMBAPATH=$(echo $ODIR | cut -d/ -f6- | perl -pe 's|/$||')
+
+cat <<EOF
+=================================================
+
+The output for ChIPSeq Project $PROJNO is now ready and is
+available on the BIC server at:
+
+    PC link: \\\\bic.mskcc.org\\$LAB
+    MAC link: smb://bic.cbio.mskcc.org/$LAB
+
+in the folder:
+
+    /RESULTS/$SAMBAPATH
+
+If you have any questions let me know.
+
+Nicholas Socci
+Bioinformatics Core
+MSKCC
+soccin@mskcc.org
+
+=======
+EOF
